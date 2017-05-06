4821cef186d131f5f40df8d9de06ba5fd6a60cb7
AymanxD <aymanmohatarem@gmail.com>
2017-05-06 09:04:29 -0400

    Updated Home Description

--- portfolio/src/Home.jsx
+++ portfolio/src/Home.jsx
@@ -11,11 +11,11 @@ export default class Info extends Component {
             Ayman Mohatarem
           </p>
           <p className="description">
-              I am a React and Node.js web developer based in Toronto.
+              A React and Node.js web developer based in Toronto.
             <br/>
-              Everything made here has been created to build upon my tenacity for solving difficult problems.
+              Everything made here is only a slice of the ideas I have in mind to learn and develop.
             <br/>
-              I seek the epitome of innovation.
+              I solve problems.
           </p>
         </div>
         <img className="Ayman" src="http://res.cloudinary.com/aymanxd/image/upload/v1493918381/Ayman2_utydxy.jpg" alt="Ayman Mohatarem"/>
